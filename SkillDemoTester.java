cb9b0c2044fe9d20c8920dcb773dc3359704b4e4
haowang0716 <haw044@ucsd.edu>
2022-02-04 00:13:10 -0800

    create skilldemo.java and a fail case

---
+++ SkillDemoTester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    @Test
+    public void subtraction() {
+        assertEquals(2, SkillDemo.minus(22, 2));
+    }
+
+    
+}
